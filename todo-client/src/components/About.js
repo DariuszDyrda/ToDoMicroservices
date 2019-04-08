845c1754f15db2d3d7826583d9fc78a2f877ac15
DariuszDyrda <darek.d.az.nie@interia.pl>
2019-04-08 16:42:36 +0200

    About page added

--- todo-client/src/components/About.js
+++ todo-client/src/components/About.js
@@ -1,47 +1,63 @@
 import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid'
+import { withStyles, Grid, Paper, List, ListItem } from '@material-ui/core'
 
 const styles = {
     root: {
         display: 'flex',
-        justifyContent: 'space-around',
-        flexWrap: 'wrap',
-        height: '87vh',
-    },
-    img: {
-        objectFit: 'contain',
-        height: '90%',
-        marginBottom: '70px',
-    },
-    title: {
-        fontSize: '46px',
-        marginTop: '70px',
-        marginBottom: '50px',
-        textTransform: 'uppercase',
-        textAlign: 'center'
-    },
-    span: {
-        fontSize: '26px',
-        textTransform: 'none',
-    },
+        textAlign: 'center',
+        marginTop: '30px',
+        flexDirection: 'column'
+      }
   }
 
 class About extends Component {
     render() {
         const { classes } = this.props;
         return (
-            <Grid item xs={12}>
-                <div className={classes.root}>
-                    <div>
-                        <p>The whole app consists of two independent microservices. Backend is powered by NodeJs, Express and MongoDB API. Frontend has been
-                            created using React alongside with Redux and React Router.
-                        </p>
-                    </div>
-                </div>
+          <div className={classes.root}>
+          <Grid container direction="column" justify="flex-start" alignItems="center">
+          <Grid container spacing={16} justify="center">
+            <Grid item xs={9} md={12}>
+              <h3>The app consists of 4 independent microservices (nginx server, API, client, database) connected using Docker-Compose.</h3>
+              <h3>Deployment to AWS is done by Travis CI.</h3>
             </Grid>
-        )
-    }
+          </Grid>
+          <Grid container spacing={16} direction="row" justify="center" alignItems="baseline">
+            <Grid item xs={9} md={3}>
+              <Paper>
+                <h3>Backend</h3>
+                <List>
+                  <ListItem>NodeJS</ListItem>
+                  <ListItem>Express</ListItem>
+                  <ListItem>Mongoose</ListItem>
+                </List>
+              </Paper>
+            </Grid>
+            <Grid item xs={9} md={3}>
+              <Paper>
+                <h3>Frontend</h3>
+                <List>
+                  <ListItem>React</ListItem>
+                  <ListItem>Redux</ListItem>
+                  <ListItem>React Router</ListItem>
+                  <ListItem>Material UI</ListItem>
+                </List>
+              </Paper>
+            </Grid>
+            <Grid item xs={9} md={3}>
+              <Paper>
+                <h3>Database</h3>
+                <List>
+                  <ListItem>Mongo container in development</ListItem>
+                  <ListItem>MongoAtlas in production</ListItem>
+                </List>
+              </Paper>
+            </Grid>
+          </Grid>
+          </Grid>
+          </div>
+        );
+      }
 }
 
 export default withStyles(styles)(About);
