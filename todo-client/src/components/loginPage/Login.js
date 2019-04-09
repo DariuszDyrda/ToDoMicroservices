60923ea0e587b7261d47f5dc1da9ad928e4fc925
DariuszDyrda <darek.d.az.nie@interia.pl>
2019-04-09 04:10:42 +0200

    Remember me checkbox deleted from Login form

--- todo-client/src/components/loginPage/Login.js
+++ todo-client/src/components/loginPage/Login.js
@@ -4,8 +4,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -152,10 +150,6 @@ class Login extends Component {
                     <InputLabel htmlFor="password">Password</InputLabel>
                     <Input name="password" type="password" id="password" autoComplete="current-password" onChange={this.handleChange} />
                 </FormControl>
-                <FormControlLabel
-                    control={<Checkbox value="remember" color="primary" />}
-                    label="Remember me"
-                />
                 <Button
                     type="submit"
                     fullWidth
